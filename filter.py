a7960f2e50558f6e8da5db267a8a76ae6d1ca9c9
billy98 <5884625@qq.com>
2017-02-13 15:01:04 +0800

    Modify the error

--- filter.py
+++ filter.py
@@ -4,7 +4,7 @@
 #回数是指从左向右读和从右向左读都是一样的数，例如12321，909。请利用filter()滤掉非回数：
 
 def is_palindrome(n):
-    return int(s             tr(n)[::-1]) == n
+    return int(str(n)[::-1]) == n
 
 output = filter(is_palindrome, range(1,1000))
 print(list(output))
